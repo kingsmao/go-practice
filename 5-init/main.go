c7a901c34756fa8e6b9d10e9fe718e0c2c78e53b
worker <>
2023-02-28 13:51:03 +0800

    多种导包方式

--- 5-init/main.go
+++ 5-init/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"go-practice/5-init/lib1"
-	"go-practice/5-init/lib2"
+	_ "go-practice/5-init/lib1" //_ 匿名导包，可以导了不用，但是可以使用里面的init函数
+	//myLib2 "go-practice/5-init/lib2" //默认名字是lib2，myLib2起别名
+	. "go-practice/5-init/lib2" //把lib2导入本类中，类似java的静态导包，不推荐使用，容易跟别的包冲突
 )
 
 func main() {
-	lib1.Lib1Test()
-	lib2.Lib2Test()
+	//lib1.Lib1Test()//不使用，但是要用lib1的init函数
+	//myLib2.Lib2Test()//别名
+	Lib2Test() //.导包
 }
